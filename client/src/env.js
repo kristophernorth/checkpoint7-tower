90e4c7d33136050d602d61967c1c25b3efae1b71
kristophernorth <kristopher.north@gmail.com>
2024-12-10 15:26:59 -0700

    auth and db hooked up

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-swk7rkkuqbbeezxr.us.auth0.com'
+export const clientId = 'lKY4G7kpWAekWkEzPw36oTVuyHfWQdEY'
+export const audience = 'https://auth.secondcomingfoundation.com'
