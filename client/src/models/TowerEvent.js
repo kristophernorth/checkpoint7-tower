1bf3ddd98cf3757af579b952b2ca62d8ccd5fd4e
kristophernorth <kristopher.north@gmail.com>
2024-12-10 19:23:57 -0700

    albums are saving to appstate

---
+++ client/src/models/TowerEvent.js
@@ -0,0 +1,17 @@
+export class TowerEvent {
+  constructor(data) {
+    this.id = data._id
+    this.capacity = data.capacity
+    this.coverImg = data.coverImg
+    this.createdAt = data.createdAt
+    this.creator = data.creator
+    this.creatorId = data.creatorId
+    this.description = data.description
+    this.isCanceled = data.isCanceled
+    this.location = data.location
+    this.name = data.name
+    this.type = data.type
+    this.startDate = new Date (data.startDate)
+    this.updatedAt = new Date(data.updatedAt)
+  }
+}
--- client/src/services/TowerEventsService.js
+++ client/src/services/TowerEventsService.js
@@ -1,12 +1,15 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { AppState } from "@/AppState.js"
+import { TowerEvent } from "@/models/TowerEvent.js"
 
 class TowerEventsService{
 
   async getTowerEvents() {
     const response = await api.get('api/events')
     logger.log('Got tower events', response.data)
-    // return towerEvents
+    const towerEvents = response.data.map(towerEventData => new TowerEvent(towerEventData))
+    AppState.towerEvents = towerEvents
 }
 }
 
